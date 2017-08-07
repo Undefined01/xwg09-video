da081454a4d4e8f0f133b1e8123896d821dbe720
Undefined01 <amoscr@163.com>
2017-08-07 17:27:38 +0800

    070809

---
+++ js/GET.js
@@ -0,0 +1,20 @@
+var $_GET;
+function load_GET()
+{
+	$_GET = (function(){
+		var url = window.document.location.href.toString();
+		var u = url.split("?");
+		if(typeof(u[1]) == "string"){
+			u = u[1].split("&");
+			var get = {};
+			for(var i in u){
+				var j = u[i].split("=");
+				get[j[0]] = j[1];
+			}
+			return get;
+		} else {
+			return {};
+		}
+	})();
+}
+load_GET();
